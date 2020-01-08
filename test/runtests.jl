93804e82c6a8864eecab33113caf37310c242774
Aurelio Amerio <aurelio.amerio@edu.unito.it>
2020-01-08 12:53:16 +0100

    cleaned up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,10 +12,6 @@ using MultiQuad, Test, Unitful
         res,
         atol = rtol * 1e2,
     )
-    # @test abs(quad(func1, xmin, xmax, rtol = rtol, method = :vegas)[1] -
-    #           1.5237995075453157) <= rtol * 1e2
-    # @test abs(quad(func1, xmin, xmax, rtol = rtol, method = :suave)[1] -
-    #           1.5288445051126394) <= rtol * 1e2
 
     xmin2 = 0 * u"m"
     xmax2 = 4 * u"m"
@@ -26,10 +22,6 @@ using MultiQuad, Test, Unitful
         res2 * u"m^3",
         atol = rtol * 1e2 * u"m^3",
     )
-    # @test abs(quad(func2, xmin2, xmax2, rtol = rtol, method = :vegas)[1] -
-    #           21.333438438582508 * u"m^3") <= rtol * 1e2 * u"m^3"
-    # @test abs(quad(func2, xmin2, xmax2, rtol = rtol, method = :suave)[1] -
-    #           21.401196340660448 * u"m^3") <= rtol * 1e2 * u"m^3"
 
     @test_throws ErrorException quad(func1, 0, 4, method = :none)
 end
@@ -50,33 +42,6 @@ end
         res1,
         atol = rtol * 1e2,
     )
-    # @test abs(dblquad(
-    #     func1,
-    #     xmin,
-    #     xmax,
-    #     ymin,
-    #     ymax,
-    #     rtol = rtol,
-    #     method = :vegas,
-    # )[1] - 48332.56784003042) <= rtol * 1e2
-    # @test abs(dblquad(
-    #     func1,
-    #     xmin,
-    #     xmax,
-    #     ymin,
-    #     ymax,
-    #     rtol = rtol,
-    #     method = :suave,
-    # )[1] - 48317.671265074176) <= rtol * 1e2
-    # @test abs(dblquad(
-    #     func1,
-    #     xmin,
-    #     xmax,
-    #     ymin,
-    #     ymax,
-    #     rtol = rtol,
-    #     method = :divonne,
-    # )[1] - 48345.25072378812) <= rtol * 1e2
 
     xmin2 = 0 * u"m"
     xmax2 = 4 * u"m"
@@ -89,33 +54,6 @@ end
         res1 * u"m^10",
         atol = rtol * 1e2 * u"m^10",
     )
-    # @test abs(dblquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     rtol = rtol,
-    #     method = :vegas,
-    # )[1] - 48332.56784003042 * u"m^10") <= rtol * 1e2 * u"m^10"
-    # @test abs(dblquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     rtol = rtol,
-    #     method = :suave,
-    # )[1] - 48317.671265074176 * u"m^10") <= rtol * 1e2 * u"m^10"
-    # @test abs(dblquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     rtol = rtol,
-    #     method = :divonne,
-    # )[1] - 48345.25072378812 * u"m^10") <= rtol * 1e2 * u"m^10"
 
     @test_throws ErrorException dblquad(
         func1,
@@ -146,40 +84,6 @@ end
         atol = rtol * 1e2,
     )
 
-    # @test abs(tplquad(
-    #     func1,
-    #     xmin,
-    #     xmax,
-    #     ymin,
-    #     ymax,
-    #     zmin,
-    #     zmax,
-    #     rtol = rtol,
-    #     method = :vegas,
-    # )[1] + 16256.948833335844) < rtol * 1e2
-    # @test abs(tplquad(
-    #     func1,
-    #     xmin,
-    #     xmax,
-    #     ymin,
-    #     ymax,
-    #     zmin,
-    #     zmax,
-    #     rtol = rtol,
-    #     method = :suave,
-    # )[1] + 16254.99067642033) < rtol * 1e2
-    # @test abs(tplquad(
-    #     func1,
-    #     xmin,
-    #     xmax,
-    #     ymin,
-    #     ymax,
-    #     zmin,
-    #     zmax,
-    #     rtol = rtol,
-    #     method = :divonne,
-    # )[1] + 16262.818635298641) < rtol * 1e2
-
     xmin2 = 0 * u"m"
     xmax2 = 4 * u"m"
     ymin2(x) = x^2 * u"m"
@@ -193,49 +97,4 @@ end
         res * u"m^10",
         atol = rtol * 1e2 * u"m^10",
     )
-    # @test abs(tplquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     zmin2,
-    #     zmax2,
-    #     rtol = rtol,
-    #     method = :vegas,
-    # )[1] + 16256.948833335844u"m^10") < rtol * 1e2u"m^10"
-    # @test abs(tplquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     zmin2,
-    #     zmax2,
-    #     rtol = rtol,
-    #     method = :suave,
-    # )[1] + 16254.99067642033u"m^10") < rtol * 1e2u"m^10"
-    # @test abs(tplquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     zmin2,
-    #     zmax2,
-    #     rtol = rtol,
-    #     method = :divonne,
-    # )[1] + 16262.818635298641u"m^10") < rtol * 1e2u"m^10"
-    #
-    # @test_throws ErrorException tplquad(
-    #     func2,
-    #     xmin2,
-    #     xmax2,
-    #     ymin2,
-    #     ymax2,
-    #     zmin2,
-    #     zmin2,
-    #     rtol = rtol,
-    #     method = :none,
-    # )
 end
