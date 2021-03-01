22801866998802d08d829944016162f7d4ffa092
Aurelio Amerio <aure.amerio@gmail.com>
2021-03-01 14:54:40 +0100

    add new integration methods for 1D integrals

--- src/MultiQuad.jl
+++ src/MultiQuad.jl
@@ -1,8 +1,125 @@
 module MultiQuad
 
-using QuadGK, Cuba, HCubature, Unitful
+using QuadGK, Cuba, HCubature, Unitful, FastGaussQuadrature, LinearAlgebra, LRUCache
 
 export quad, dblquad, tplquad
+
+
+function _quadgk_wrapper_1D(arg::Function, x1, x2; kwargs...)
+    return quadgk(arg, x1, x2; kwargs...)
+end
+
+function _cuba_wrapper_1D(arg::Function, x1, x2; method::Symbol, kwargs...)
+    if method == :suave
+        integrate = suave
+    elseif method == :vegas
+        integrate = vegas
+    else
+        ex = ErrorException("Integration method $method is not supported!")
+        throw(ex)
+    end
+
+    units = unit(arg(x1)) * unit(x1)
+
+    arg2(a) = ustrip(units, (x2 - x1) * arg((x2 - x1) * a + x1))::Float64
+
+    function integrand(x, f)
+        f[1] = arg2(x[1])
+    end
+
+    result, err = integrate(integrand, 1, 1; kwargs...)
+
+    if units == Unitful.NoUnits
+        return result[1], err[1]
+    else
+        return (result[1], err[1]) .* units
+    end
+end
+
+#order, method, optional
+
+function get_weights_fastgaussquadrature(order::Int, method::Symbol, optional::Real=NaN)
+    if method == :gausslegendre
+        xw = gausslegendre
+        return xw(order)
+
+    elseif method == :gausshermite
+        xw = gausshermite
+        return xw(order)
+
+    elseif method == :gausslaguerre
+        xw = gausslaguerre
+        if isnan(optional)
+            return xw(order)
+        else
+            α = optional
+            return xw(order, α)
+        end
+
+    elseif method == :gausschebyshev
+        xw = gausschebyshev
+        if isnan(optional)
+            return xw(order)
+        else
+            kind = optional
+            return xw(order, kind)
+        end
+
+    # elseif method == :gaussjacobi
+    #     xw = gaussjacobi
+    #     return xw(order)
+
+    elseif method == :gaussradau
+        xw = gaussradau
+        return xw(order)
+
+    elseif method == :gausslobatto
+        xw = gausslobatto
+        return xw(order)
+    
+    else
+        ex = ErrorException("Integration method $method is not supported!")
+        throw(ex)
+    end
+
+    
+end
+
+const lru_xw_fastgaussquadrature = LRU{
+    Tuple{Int,Symbol,Real},
+    Tuple{Vector{Float64},Vector{Float64}},
+}(maxsize = Int(1e5))
+
+function get_weights_fastgaussquadrature_cached(order::Int, method::Symbol, optional::Real=NaN)
+    get!(lru_xw_fastgaussquadrature, (order, method, optional)) do
+        get_weights_fastgaussquadrature(order, method, optional)
+    end
+end
+
+function _fastgaussquadrature_wrapper_1D(arg::Function, x1, x2; method::Symbol, order::Int, kwargs...)
+    
+    if haskey(kwargs, :α)
+        optional = kwargs[:α]
+    elseif haskey(kwargs, :kind)
+        optional = kwargs[:kind]
+    else 
+        optional = NaN
+    end
+
+    x, w = get_weights_fastgaussquadrature_cached(order, method, optional)
+
+    b=x2
+    a=x1
+
+    b_a_2 = (b-a)/2
+    a_plus_b_2 = (a+b)/2
+
+    arg_gauss(x) = arg(b_a_2*x + a_plus_b_2)
+    
+    integral = b_a_2 * dot(w, arg_gauss.(x))
+    return integral, NaN
+
+end
 
 @doc raw"""
     quad(arg::Function, x1, x2; method = :quadgk, kwargs...)
@@ -31,32 +148,73 @@ func(x) = x^2
 integral, error = quad(func, 1u"m", 5u"m")
 ```
 """
-function quad(arg::Function, x1, x2; method = :quadgk, kwargs...)
+function quad(arg::Function, x1, x2; method::Symbol = :quadgk, order::Int=1000, kwargs...)
 
-    if method == :suave
+    if method in [:suave, :vegas]
+        return _cuba_wrapper_1D(arg, x1, x2; method=method)
+    elseif method == :quadgk
+        return _quadgk_wrapper_1D(arg, x1, x2; kwargs...)
+    elseif method in [:gausslegendre, :gausshermite, :gausslaguerre, :gausschebyshev, :gaussjacobi, :gaussradau, :gausslobatto]
+        return _fastgaussquadrature_wrapper_1D(arg, x1, x2; method=method, order=order, kwargs...)    
+    else
+        ex = ErrorException("Integration method $method is not supported!")
+        throw(ex)
+    end
+end
+
+
+function _cuba_wrapper_2D(arg::Function, x1, x2, y1::Function, y2::Function; method::Symbol, kwargs...)
+    if method == :cuhre
+        integrate = cuhre
+    elseif method == :divonne
+        integrate = divonne
+    elseif method == :suave
         integrate = suave
     elseif method == :vegas
         integrate = vegas
-    elseif method == :quadgk
-        integrate = quadgk
     else
         ex = ErrorException("Integration method $method is not supported!")
         throw(ex)
     end
 
-    if method == :quadgk
-        return quadgk(arg, x1, x2; kwargs...)
+    units = unit(arg(y1(x1), x1)) * unit(x1) * unit(y1(x1))
+
+    arg1(a, x) = (y2(x) - y1(x)) * arg((y2(x) - y1(x)) * a + y1(x), x)
+
+
+    arg2(a, b) = ustrip(units, (x2 - x1) * arg1(a, (x2 - x1) * b + x1))::Float64
+
+    function integrand2(x, f)
+        f[1] = arg2(x[1], x[2])
     end
 
-    units = unit(arg(x1)) * unit(x1)
+    result, err = integrate(integrand2, 2, 1; kwargs...)
 
-    arg2(a) = ustrip(units, (x2 - x1) * arg((x2 - x1) * a + x1))::Float64
+    if units == Unitful.NoUnits
+        return result[1], err[1]
+    else
+        return (result[1], err[1]) .* units
+    end
+end
 
-    function integrand(x, f)
-        f[1] = arg2(x[1])
+
+function _hcubature_wrapper_2D(arg::Function, x1, x2, y1::Function, y2::Function; kwargs...)
+    units = unit(arg(y1(x1), x1)) * unit(x1) * unit(y1(x1))
+
+    arg1(a, x) = (y2(x) - y1(x)) * arg((y2(x) - y1(x)) * a + y1(x), x)
+
+
+    arg2(a, b) = ustrip(units, (x2 - x1) * arg1(a, (x2 - x1) * b + x1))::Float64
+
+
+    function integrand(arr)
+        return arg2(arr[1], arr[2])
     end
 
-    result, err = integrate(integrand, 1, 1; kwargs...)
+    min_arr = [0, 0]
+    max_arr = [1, 1]
+    result, err = hcubature(integrand, min_arr, max_arr; kwargs...)
+    
 
     if units == Unitful.NoUnits
         return result[1], err[1]
@@ -105,6 +263,17 @@ function dblquad(
     kwargs...,
 )
 
+    if method in [:cuhre, :divonne, :suave, :vegas]
+        return _cuba_wrapper_2D(arg, x1, x2, y1, y2; method=method)
+    elseif method == :hcubature
+        return _hcubature_wrapper_2D(arg, x1, x2, y1, y2; kwargs...)
+    else
+        ex = ErrorException("Integration method $method is not supported!")
+        throw(ex)
+    end
+end
+
+function _cuba_wrapper_3D(arg::Function, x1, x2, y1::Function, y2::Function, z1::Function, z2::Function;  method::Symbol, kwargs...)
     if method == :cuhre
         integrate = cuhre
     elseif method == :divonne
@@ -113,36 +282,61 @@ function dblquad(
         integrate = suave
     elseif method == :vegas
         integrate = vegas
-    elseif method == :hcubature
-        integrate = hcubature
     else
         ex = ErrorException("Integration method $method is not supported!")
         throw(ex)
     end
 
-    units = unit(arg(y1(x1), x1)) * unit(x1) * unit(y1(x1))
+    units = unit(arg(z1(x1, y1(x1)), y1(x1), x1)) * unit(x1) * unit(y1(x1)) *
+            unit(z1(y1(x1), x1))
 
-    arg1(a, x) = (y2(x) - y1(x)) * arg((y2(x) - y1(x)) * a + y1(x), x)
+    arg0(a, y, x) =
+        (z2(x, y) - z1(x, y)) * arg((z2(x, y) - z1(x, y)) * a + z1(x, y), y, x)
+
+    arg1(a, b, x) = (y2(x) - y1(x)) * arg0(a, (y2(x) - y1(x)) * b + y1(x), x)
 
 
-    arg2(a, b) = ustrip(units, (x2 - x1) * arg1(a, (x2 - x1) * b + x1))::Float64
+    arg2(a, b, c) =
+        ustrip(units, (x2 - x1) * arg1(a, b, (x2 - x1) * c + x1))::Float64
 
-    if method == :hcubature
-        function integrand(arr)
-            return arg2(arr[1], arr[2])
-        end
-
-        min_arr = [0, 0]
-        max_arr = [1, 1]
-        result, err = integrate(integrand, min_arr, max_arr; kwargs...)
-    else
-        function integrand2(x, f)
-            f[1] = arg2(x[1], x[2])
-        end
-
-        result, err = integrate(integrand2, 2, 1; kwargs...)
+    
+    function integrand2(x, f)
+        f[1] = arg2(x[1], x[2], x[3])
     end
 
+    result, err = integrate(integrand2, 3, 1; kwargs...)
+    
+
+    if units == Unitful.NoUnits
+        return result[1], err[1]
+    else
+        return (result[1], err[1]) .* units
+    end
+end
+
+function _hcubature_wrapper_3D(arg::Function, x1, x2, y1::Function, y2::Function, z1::Function, z2::Function; kwargs...)
+
+    units = unit(arg(z1(x1, y1(x1)), y1(x1), x1)) * unit(x1) * unit(y1(x1)) *
+            unit(z1(y1(x1), x1))
+
+    arg0(a, y, x) =
+        (z2(x, y) - z1(x, y)) * arg((z2(x, y) - z1(x, y)) * a + z1(x, y), y, x)
+
+    arg1(a, b, x) = (y2(x) - y1(x)) * arg0(a, (y2(x) - y1(x)) * b + y1(x), x)
+
+
+    arg2(a, b, c) =
+        ustrip(units, (x2 - x1) * arg1(a, b, (x2 - x1) * c + x1))::Float64
+
+    
+    function integrand(arr)
+        return arg2(arr[1], arr[2], arr[3])
+    end
+
+    min_arr = [0, 0, 0]
+    max_arr = [1, 1, 1]
+    result, err = hcubature(integrand, min_arr, max_arr; kwargs...)
+    
     if units == Unitful.NoUnits
         return result[1], err[1]
     else
@@ -188,57 +382,19 @@ function tplquad(
     y2::Function,
     z1::Function,
     z2::Function;
-    method = :cuhre,
+    method = :hcubature,
     kwargs...,
 )
 
-    if method == :cuhre
-        integrate = cuhre
-    elseif method == :divonne
-        integrate = divonne
-    elseif method == :suave
-        integrate = suave
-    elseif method == :vegas
-        integrate = vegas
+    if method in [:cuhre, :divonne, :suave, :vegas]
+        return _cuba_wrapper_3D(arg, x1, x2, y1, y2, z1, z2;  method=method, kwargs...)
+
     elseif method == :hcubature
-        integrate = hcubature
+        return _hcubature_wrapper_3D(arg, x1, x2, y1, y2, z1, z2; kwargs...)
+
     else
         ex = ErrorException("Integration method $method is not supported!")
         throw(ex)
-    end
-
-    units = unit(arg(z1(x1, y1(x1)), y1(x1), x1)) * unit(x1) * unit(y1(x1)) *
-            unit(z1(y1(x1), x1))
-
-    arg0(a, y, x) =
-        (z2(x, y) - z1(x, y)) * arg((z2(x, y) - z1(x, y)) * a + z1(x, y), y, x)
-
-    arg1(a, b, x) = (y2(x) - y1(x)) * arg0(a, (y2(x) - y1(x)) * b + y1(x), x)
-
-
-    arg2(a, b, c) =
-        ustrip(units, (x2 - x1) * arg1(a, b, (x2 - x1) * c + x1))::Float64
-
-    if method == :hcubature
-        function integrand(arr)
-            return arg2(arr[1], arr[2], arr[3])
-        end
-
-        min_arr = [0, 0, 0]
-        max_arr = [1, 1, 1]
-        result, err = integrate(integrand, min_arr, max_arr; kwargs...)
-    else
-        function integrand2(x, f)
-            f[1] = arg2(x[1], x[2], x[3])
-        end
-
-        result, err = integrate(integrand2, 3, 1; kwargs...)
-    end
-
-    if units == Unitful.NoUnits
-        return result[1], err[1]
-    else
-        return (result[1], err[1]) .* units
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -45,6 +45,25 @@ using MultiQuad, Test, Unitful
         method = :suave,
     )[1]) == typeof(1.0 * u"m^3")
 
+
+    f(x) = x^4
+
+    @test quad(f, -1, 1, method=:gausslegendre, order=100000)[1] ≈ 2/5
+
+    @test quad(f, -1, 1, method=:gausshermite, order=10000)[1] ≈ 3(√π)/4
+
+    @test quad(f, -1, 1, method=:gausslaguerre, order=10000)[1] ≈ 24
+    @test quad(f, -1, 1, method=:gausslaguerre, order=3, α=1.0)[1] ≈ 120
+
+    @test quad(f, -1, 1, method=:gausschebyshev, order=3)[1] ≈ 3π/8
+    @test quad(f, -1, 1, method=:gausschebyshev, order=3, kind=1)[1] ≈ 3π/8
+    @test quad(f, -1, 1, method=:gausschebyshev, order=3, kind=2)[1] ≈ π/16
+    @test quad(f, -1, 1, method=:gausschebyshev, order=3, kind=3)[1] ≈ 3π/8
+    @test quad(f, -1, 1, method=:gausschebyshev, order=3, kind=4)[1] ≈ 3π/8
+
+    @test quad(f, -1, 1, method=:gaussradau, order=3)[1] ≈ 2/5
+    @test quad(f, -1, 1, method=:gausslobatto, order=4)[1] ≈ 2/5
+
     @test_throws ErrorException quad(func1, 0, 4, method = :none)
 end
 
